ecd306620bcc75fa2d8255c668fa34b777c012bf
Beatrice Tan <113320828+beartisan@users.noreply.github.com>
2022-12-02 20:44:08 -0500

    Add files via upload

---
+++ Models/SchoolDbContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+namespace CumulativeProject.Models
+{
+    public class SchoolDbContext
+    {
+        private static string User { get { return "root"; } }
+
+        private static string Password { get { return "root"; } }
+
+        private static string Database { get { return "SchoolDb"; } }
+
+        private static string Server { get { return "localhost"; } }
+
+        private static string Port { get { return "3306"; } }
+
+        //ConnectionString
+        protected static string ConnectionString
+        {
+            get
+            {
+                //convert zero datetime is a db connection
+
+                return "server = " + Server
+                    + "; user = " + User
+                    + "; database = " + Database
+                    + "; port = " + Port
+                    + "; password = " + Password
+                    + "; convert zero datetime = True";
+            }
+        }
+
+        ///<summary>
+        ///Returns a connection to the blog database
+        ///</summary>
+        ///<example>
+        ///private SchoolDbContext School = new SchoolDbContext();
+        ///MySqlConnection Conn = School.AccessDatabase();
+        /// </example>
+        /// <returns>A MySqlConnection Object</returns>
+
+        public MySqlConnection AccessDatabase()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+    }
+}
---
+++ Models/teacher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace CumulativeProject.Models
+{
+    public class Teacher
+    {
+        //fields that define Teacher's employment profile 
+        public int TeacherId;
+        public string TeacherFname;
+        public string TeacherLname;
+        public string EmployeeNumber;
+        public DateTime HireDate;
+        public decimal Salary; 
+
+    }
+}
